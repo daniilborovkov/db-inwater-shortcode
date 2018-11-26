57d5ae280539543ec263bf4c47b194b7d4b3f0ab
Daniil Borovkov <daniilborowkow@gmail.com>
2018-11-26 19:11:17 +0600

    Initial commit

---
+++ db-inwater-shortcode.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Inwater Shortcode
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Inwater shortcode plugin
+ * Author:          Daniil Borovkov
+ * Author URI:      https://daniilborovkov.site
+ * Text Domain:     db-inwater-shortcode
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Db_Inwater_Shortcode
+ */
+
+// Your code starts here.
